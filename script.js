7d8704289aa155a0668ea0f923ef75dae17c5f35
Caio Henrique Almeida Ferreira Santos <caio_hafs2006@outlook.com>
2021-10-29 13:55:32 -0300

    mudança no arquivo json

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-var requestJSON = 'data_bank.json';
+var requestJSON = 'https://raw.githubusercontent.com/caio-henrique2006/Cakios-book-list/main/data_bank.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestJSON);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var list = request.response;
+  books(list);
+}
+
+function books(estante){
+	console.log(estante['title']);
+}

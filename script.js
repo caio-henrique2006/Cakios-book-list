de6a662b5fc1cdd2223fda2cffea6513ae523042
Caio Henrique Almeida Ferreira Santos <caio_hafs2006@outlook.com>
2021-11-14 08:14:29 -0300

    Atualizacao

--- script.js
+++ script.js
@@ -13,25 +13,30 @@ request.onload = function() {
 
 function books(estante){
 	for(var i = 0; i < estante.length; i++){
+		// Criando todos os elementos HTML que vou usar:
 		const br = document.createElement("br");
 		const image = document.createElement("img");
 		const p_title = document.createElement("a");
 		const p_author = document.createElement("a");
 		const div = document.createElement("div");
 
+		// Criando e colocando texto nos parágrafos:
 		const text_title = document.createTextNode(estante[i].title);
 		const text_author = document.createTextNode(estante[i].author);
 		p_title.appendChild(text_title);
 		p_author.appendChild(text_author);
 
+		// Definindo um div para colocar os elementos:
 		const div_result = document.getElementById("result");
 		div_result.appendChild(div);
 
+		// Colocando todos os elementos no div:
 		div.appendChild(image);
 		div.appendChild(p_title);
 		div.appendChild(br);
 		div.appendChild(p_author);
 		
+		// Setando atributos:
 		image.setAttribute("src", estante[i].image);
 		image.setAttribute("class", "class_img");
 		div.setAttribute("class", "class_div");
